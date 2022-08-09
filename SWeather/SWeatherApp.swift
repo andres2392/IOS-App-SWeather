88ce475af33ca45ee7281b7b0dc83d0618a91cf0
Andres <andres.gonzalez2392@gmail.com>
2022-08-09 16:13:55 -0400

    Initial Commit

---
+++ SWeather/SWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWeatherApp.swift
+//  SWeather
+//
+//  Created by PDWS on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct SWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
